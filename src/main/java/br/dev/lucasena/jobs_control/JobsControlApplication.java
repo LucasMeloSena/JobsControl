23ff9763d86871b05e61fc7e6db0bb4c5724bd94
Lucas Sena <lucasmelosena@gmail.com>
2024-12-12 16:00:51 -0300

    Initial commit

---
+++ src/main/java/br/dev/lucasena/jobs_control/JobsControlApplication.java
@@ -0,0 +1,13 @@
+package br.dev.lucasena.jobs_control;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobsControlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobsControlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/dev/lucasena/jobs_control/JobsControlApplicationTests.java
@@ -0,0 +1,13 @@
+package br.dev.lucasena.jobs_control;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobsControlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
